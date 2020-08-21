ddc05aaeef37adc9a9648cbf694c40d2587c3258
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2020-08-21 12:13:16 -0700

    sign up validation has been implemented as well as a modal display each time a new account is successfully created

--- blue-saucier/src/components/SignUp.js
+++ blue-saucier/src/components/SignUp.js
@@ -1,60 +1,146 @@
 import React from 'react';
 import fire from '../config/firebase';
 
+import SuccessModal from './Management/SuccessModal';
 import Footer from './Footer';
+
+const initialState = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+
+  firstNameError: '',
+  lastNameError: '',
+  emailError: '',
+  passwordError: '',
+
+  modalOpen: false
+}
 
 class SignUp extends React.Component {
 
-    state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
+    state = initialState;
+
+    validate = () => {
+     let firstNameError = '';
+     let lastNameError = '';
+     let emailError = '';
+     let passwordError = ''
+
+     if(!this.state.firstName){
+       firstNameError = 'Please provide your first name'
+     }
+
+     if(!this.state.lastName){
+      firstNameError = 'Please provide your last name'
+    }
+
+    if(!this.state.email){
+      firstNameError = 'Please provide an email adress'
+    }
+
+    if(!this.state.password){
+      firstNameError = 'Please provide a password'
+    }
+
+     if(firstNameError || lastNameError || emailError || passwordError){
+       this.setState({ firstNameError, lastNameError, emailError, passwordError });
+       return false;
+     }
+
+     return true;
     }
 
       handleSubmit = event => {
         event.preventDefault();
+
+        const isValid = this.validate();
+        if(isValid){
           fire
           .auth()
           .createUserWithEmailAndPassword(this.state.email, this.state.password)
           .catch(error => {
             console.log('error',error);
           });
+          console.log(this.state)
+          this.setState(initialState);
+          this.setState({modalOpen: true})
+        }
       }
-    
+      
       handleChanges= event => {
         this.setState({[event.target.name]: event.target.value});
-
       }
     
    
   render() {
     return(
-      <div>
-       <form onSubmit={this.handleSubmit} className="signUp ui form">
+      <>
+        <SuccessModal 
+          key='modal1'
+          modalOpen={this.state.modalOpen}
+        />
+
+       <form onSubmit={this.handleSubmit} className="signUp ui form error">
          <h2 className='instructions'>Please provide the following information to create an account.</h2>
          <div className='formContent'>
-          <div className='field four wide field'>
-            <label style={{color: 'white'}}>First Name</label>
-            <input value={this.state.firstName} onChange={this.handleChanges} type='text' name='firstName' placeholder='First Name' autoComplete='off'/>
-          </div>
+
+            <div className='ui error message' style={{width:'25%'}}>{this.state.firstNameError}</div>
+            <div className='field four wide field'>
+              <label style={{color: 'white'}}>First Name</label>
+              <input 
+                value={this.state.firstName} 
+                onChange={this.handleChanges} 
+                type='text' 
+                name='firstName' 
+                placeholder='First Name' 
+                autoComplete='off'
+              />
+              
+            </div>
+
           <div className='field four wide field'>
             <label style={{color: 'white'}}>Last Name</label>
-            <input value={this.state.lastName} onChange={this.handleChanges} type='text' name='lastName' placeholder='Last Name' autoComplete='off'/>
+            <input 
+              value={this.state.lastName} 
+              onChange={this.handleChanges} 
+              type='text' name='lastName' 
+              placeholder='Last Name' 
+              autoComplete='off'
+            />
+            <div className='ui error message'>{this.state.lastNameError}</div>
           </div>
+
           <div className="field four wide field">
             <label style={{color: 'white'}}>E-mail</label>
-            <input value={this.state.email} onChange={this.handleChanges} type="email" name='email' placeholder="Email" autoComplete='off'/>
+            <input 
+              value={this.state.email} 
+              onChange={this.handleChanges} 
+              type="email" name='email' 
+              placeholder="Email" 
+              autoComplete='off'
+            />
+            <div className='ui error message'>{this.state.emailError}</div>
           </div>
+
           <div className="field four wide field">
             <label style={{color: 'white'}}>Password</label>
-            <input value={this.state.password} onChange={this.handleChanges} type="password" name='password' placeholder='Password' autoComplete='off'/>
+            <input 
+              value={this.state.password} 
+              onChange={this.handleChanges} 
+              type="password" 
+              name='password' 
+              placeholder='Password' 
+              autoComplete='off'
+            />
+            <div className='ui error message'>{this.state.passwordError}</div>
           </div>
           <button className="ui button" type="submit" style={{marginLeft: '8%'}}>Submit</button>
          </div>
         </form>
         <Footer/> 
-      </div>  
+      </>  
     )
   }
 }
