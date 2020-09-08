c907217ebd229a22ac7324c55326b6d7a042e408
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2020-09-08 09:23:01 -0700

    about component code has been organized

--- blue-saucier/src/components/Home&Auth/About.js
+++ blue-saucier/src/components/Home&Auth/About.js
@@ -16,35 +16,35 @@ const About = () => {
         <Link to ='/' className='active item'>Home</Link>
         <Link to ='/log-in' className='item'>Log In</Link>
       </div>
-     <div className='ui container aboutContent'>
-      <h2 className='aboutTitle'>Blue Saucier App</h2>
-      <div className='ui segment'>
-        <div className='ui floated header' style={{fontSize: '1.5rem', paddingLeft: '33%'}}>Usefull features to improve your productivity!</div>
-        <div className='ui clearing divider'></div>
+      <div className='ui container aboutContent'>
+        <h2 className='aboutTitle'>Blue Saucier App</h2>
+        <div className='ui segment'>
+          <div className='ui floated header' style={{fontSize: '1.5rem', paddingLeft: '33%'}}>Usefull features to improve your productivity!</div>
+          <div className='ui clearing divider'/>
           <p style={{fontSize: '1.3rem'}}>Blue Saucier is a dynamic and user friendly application. It's primary goal is to assist in basic, yet imporant kitchen management tasks and menu planning. </p>
           <div className='aboutPhotoContainer'>
-          <img className='aboutPhotos' src={aboutOne} alt='two cooks in a kitchen'/>
-          <img className='aboutPhotos'src={aboutTwo} alt='two brown pages'/>
-          <img className='aboutPhotos'src={aboutThree} alt='woman writting on paper'/>
+            <img className='aboutPhotos' src={aboutOne} alt='two cooks in a kitchen'/>
+            <img className='aboutPhotos'src={aboutTwo} alt='two brown pages'/>
+            <img className='aboutPhotos'src={aboutThree} alt='woman writting on paper'/>
           </div>
         </div>
-      <div className='appDescription'>
-        <div className='ui card' style={{textAlign: 'center', marginTop:'1.2%'}}>
-          <div className='content'>
-            <div className='header'>Item List</div>
-            <div className='description' style={{marginLeft:'0'}}>
-             <ul>
-               <li>You will be able to create multiple ordering lists to help you keep track of your kitchen needs.</li> 
-                <br></br>
-               <li>Created lists can be edited for corrections or can be deleted when no longer needed.</li>
-             </ul>
+        <div className='appDescription'>
+          <div className='ui card' style={{textAlign: 'center', marginTop:'1.2%'}}>
+            <div className='content'>
+              <div className='header'>Item List</div>
+              <div className='description' style={{marginLeft:'0'}}>
+                <ul>
+                  <li>You will be able to create multiple ordering lists to help you keep track of your kitchen needs.</li> 
+                    <br></br>
+                  <li>Created lists can be edited for corrections or can be deleted when no longer needed.</li>
+                </ul>
+              </div>
+            </div>
+            <div className="image">
+              <img src={aboutList} alt='Several cooking ingredients'/>
             </div>
           </div>
-          <div className="image">
-            <img src={aboutList} alt='Several cooking ingredients'/>
-          </div>
-        </div>
-        <div className='ui card' style={{textAlign: 'center'}}>
+          <div className='ui card' style={{textAlign: 'center'}}>
           <div className='content'>
             <div className='header'>Menu Planning</div>
             <div className='description' style={{marginLeft:'0'}}>
@@ -59,20 +59,20 @@ const About = () => {
             <img src={menuPlanning} alt='a food menu'/>
           </div>
         </div>
-        <div className='ui card' style={{textAlign: 'center'}}>
-          <div className='content'>
-            <div className='header'>Google Calendar</div>
-            <div className='description' style={{marginLeft:'0'}}>
-              <ul>
-                <li>We added Google calendar and it's features to help you keep track of important events and dates.</li>
-              </ul>
+          <div className='ui card' style={{textAlign: 'center'}}>
+            <div className='content'>
+              <div className='header'>Google Calendar</div>
+              <div className='description' style={{marginLeft:'0'}}>
+                <ul>
+                  <li>We added Google calendar and it's features to help you keep track of important events and dates.</li>
+                </ul>
+              </div>
+            </div>
+            <div className="image">
+              <img src={googleCalendar} alt='Google calendar'/>
             </div>
           </div>
-          <div className="image">
-            <img src={googleCalendar} alt='Google calendar'/>
-          </div>
-        </div>
-        <div className='ui card' style={{textAlign: 'center'}}>
+          <div className='ui card' style={{textAlign: 'center'}}>
           <div className='content'>
             <div className='header'>Profile Management</div>
             <div className='description' style={{marginLeft:'0'}}>
--- blue-saucier/src/components/Home&Auth/Footer.js
+++ blue-saucier/src/components/Home&Auth/Footer.js
@@ -3,19 +3,20 @@ import {Link} from 'react-router-dom';
 
 const Footer = (props) => {
   const {menuOne, menuTwo, linkOne, linkTwo} = props;
-    return (
+  
+  return (
     <div className='footer'>
       <div className='ui two item ui inverted segment ui mini menu'>
-          <Link to ={linkOne} className='blue button item'>
-            {menuOne}
-          </Link>
-          <Link to = {linkTwo} className= 'button item'>
-            {menuTwo}
-          </Link>
+        <Link to ={linkOne} className='blue button item'>
+          {menuOne}
+        </Link>
+        <Link to = {linkTwo} className= 'button item'>
+          {menuTwo}
+        </Link>
       </div>
     </div>
-    )
-};
+    );
+}
 
 
 export default Footer;
