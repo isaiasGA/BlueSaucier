36b801183d49b66c6e7833d0629739f9df11dfc6
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2020-09-10 11:26:50 -0700

    CSS styles for home component have been re organized and the buttons have been reformated to vanilla CSS. Media queries for home component have been created to fit mobile screens

--- blue-saucier/src/components/Home&Auth/Home.js
+++ blue-saucier/src/components/Home&Auth/Home.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
 import Footer from './Footer';
 
+
 function Home(){
+
+  const history = useHistory();
+
+  const routeToSignUp = () => {
+    history.push('/sign-up')
+  }
+
+  const routeToLogIn = () => {
+    history.push('/log-in')
+  }
+
     return(
       <>
        <div className='home'>
-        <div className='home-content'>
-          <h1 className='home-title'>Blue Saucier</h1>
-            <Link className=' massive ui button ui inverted button' to='/sign-up' style={{ marginRight: '23%'}}>Sign Up</Link>
-            <Link className='massive ui button ui inverted button' to='/log-in'>Log In</Link>
+        <div className='homeContent'>
+          <h1 className='homeTitle'>Blue Saucier</h1>
+          <div className='homeBttnsContainer'>
+            <button className='homeBttns' onClick={routeToSignUp}>Sign Up</button>
+            <button className='homeBttns' onClick={routeToLogIn}>Log In</button>
+          </div>
         </div>
       </div>
       <Footer  menuOne='Contact' menuTwo='About' linkOne='/contact' linkTwo='/about'/>
