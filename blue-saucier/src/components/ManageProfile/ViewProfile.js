661e0e55400483ed84e2b1e34565744dd88c0f59
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2020-09-13 11:43:23 -0700

    styles for profile component have been reforamtted and media queries for mobile screens have been created

--- blue-saucier/src/components/ManageProfile/ViewProfile.js
+++ blue-saucier/src/components/ManageProfile/ViewProfile.js
@@ -21,18 +21,18 @@ class ManageProfile extends React.Component {
 
   render(){
     return(
-      <div className='manageProfile'>
+      <div className='viewProfile'>
         <div className='ui inverted menu' style={{margin: '0', borderRadius: '0'}}>
           <Link to ='/main-menu' className='active item'>Main Menu</Link>
         </div>
 
-        <div className='manageProfileContent'>
-          <div className='ui card' style={{marginLeft:'34%', width:'35%', height:'auto'}}>
-            <div className='manageProfileImg'>
-              <p className='editPara' style={{marginTop:'14%'}}><strong>Name:</strong> &nbsp; {this.state.displayName}</p>
-              <p className='editPara'><strong>Email:</strong>  &nbsp; {this.state.email}</p>
-              <p className='editPara'><strong>Password:</strong> &nbsp; *********</p>
-              <div className='manageButtons'>
+        <div className='viewProfileContent'>
+          <div className='ui card ui container profileContainer' style={{marginLeft:'34%', width:'35%', height:'auto'}}>
+            <div className='viewProfileImg'>
+              <p className='viewProfileInfo' style={{marginTop:'14%'}}><strong>Name:</strong> &nbsp; {this.state.displayName}</p>
+              <p className='viewProfileInfo'><strong>Email:</strong>  &nbsp; {this.state.email}</p>
+              <p className='viewProfileInfo'><strong>Password:</strong> &nbsp; *********</p>
+              <div className='viewProfileBttn'>
                 <Link to='/edit-profile' className='ui blue button'>Edit Profile</Link>
               </div>
             </div>           
